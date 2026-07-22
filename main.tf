6ff409b3da53bc75424d80caa79b840aac5db3c4
Pedro Valenzuela <pedrovalenzuela@MacBook-Pro-de-Pedro-2.local>
2026-07-22 12:32:37 -0400

    agregué funcionalidad para creación de ALB, 2 subnets públicas, y deja el acceso limitado al servidor a el ALB

--- main.tf
+++ main.tf
@@ -30,6 +30,40 @@ resource "aws_subnet" "main" {
   }
 }
 
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_eip" "elastic_ip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.elastic_ip.id
+  subnet_id = aws_subnet.main.id
+  depends_on = [aws_internet_gateway.igw]
+  
+  tags = {
+    Name = "ngw"
+  }
+}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -52,9 +86,33 @@ resource "aws_route_table" "route-table" {
 
 }
 
+resource "aws_route_table" "route-table-2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = "route-table-2"
+  }
+
+}
+
 resource "aws_route_table_association" "example" {
   subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.route-table.id
+}
+
+resource "aws_route_table_association" "example3" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.route-table.id
+}
+
+resource "aws_route_table_association" "example2" {
+  subnet_id      = aws_subnet.private_subnet_a.id
+  route_table_id = aws_route_table.route-table-2.id
 }
 
 resource "aws_security_group" "sg_web" {
@@ -85,13 +143,93 @@ resource "aws_security_group" "sg_web" {
   }
 }
 
+resource "aws_security_group" "sg_lb" {
+  vpc_id = aws_vpc.main.id
+  description = "Permite HTTP desde internet"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_ec2" {
+  name   = "ec2-security-group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description     = "HTTP solo desde el ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_lb.id] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_instance" "servidor" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.sg_web.id]
-  subnet_id = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+  subnet_id = aws_subnet.private_subnet_a.id
 
   tags = {
     Name = "servidor-sg-web"
+  }
+}
+
+resource "aws_lb" "alb" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_lb.id]
+  subnets            = [aws_subnet.main.id, aws_subnet.public_subnet_a.id]
+
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name     = "tf-example-lb-nlb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  target_health_state {
+    enable_unhealthy_connection_termination = false
+  }
+}
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.servidor.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.alb.arn
+  protocol = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
 }
